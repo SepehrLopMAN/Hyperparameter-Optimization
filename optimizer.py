c5a9670c672530b098cde2c8ec80aa1f4e760d0a
Sepehr <sepehrkatebi61@gmail.com>
2026-03-17 16:57:41 +0400

    Patch commit 0.1.1 > PSO algorithm added (Optimizer.py modified)

--- optimizer.py
+++ optimizer.py
@@ -1,33 +1,58 @@
 import torch
-
+# Particle Swarm Optimization Algorithm Optimizer class
 class Optimizer:
 
-    def __init__(self, pop_size, dim, lower, upper, max_fes, device):
+    def __init__(self, pop_size, dim, lower, upper, max_fes, device,
+                 w=0.7, c1=1.5, c2=1.5):
+
         self.pop_size = pop_size
         self.dim = dim
         self.lower = lower
         self.upper = upper
         self.max_fes = max_fes
         self.device = device
+        self.w = w
+        self.c1 = c1
+        self.c2 = c2
 
     def optimize(self, func):
 
-        # Initialize population on GPU
-        population = torch.empty(
-            self.pop_size, self.dim, device=self.device
-        ).uniform_(self.lower, self.upper)
+        pos = torch.empty(self.pop_size, self.dim, device=self.device)\
+            .uniform_(self.lower, self.upper)
 
-        FEs = 0
-        best_value = float("inf")
+        vel = torch.zeros_like(pos)
+
+        pbest = pos.clone()
+        pbest_val = func(pos)
+
+        FEs = self.pop_size
+        gbest_val, g_idx = torch.min(pbest_val, dim=0)
+        gbest = pbest[g_idx]
+
+        best_value = gbest_val.item()
 
         while FEs < self.max_fes:
 
-            fitness = func(population)
+            r1 = torch.rand_like(pos)
+            r2 = torch.rand_like(pos)
+
+            vel = (self.w * vel +
+                   self.c1 * r1 * (pbest - pos) +
+                   self.c2 * r2 * (gbest - pos))
+
+            pos = pos + vel
+            pos = torch.clamp(pos, self.lower, self.upper)
+
+            fitness = func(pos)
             FEs += self.pop_size
 
-            current_best = torch.min(fitness).item()
-            best_value = min(best_value, current_best)
+            better = fitness < pbest_val
+            pbest[better] = pos[better]
+            pbest_val[better] = fitness[better]
 
-            ### Algorithm to be added 
+            gbest_val, g_idx = torch.min(pbest_val, dim=0)
+            gbest = pbest[g_idx]
+
+            best_value = min(best_value, gbest_val.item())
 
         return best_value
