48d94046fde02f6f58618cd0553e1d2019b3a81f
Sepehr <sepehrkatebi61@gmail.com>
2026-03-17 16:39:48 +0400

    Patch commit 0.1.1 > GWO algorithm added (Optimizer.py modified)

--- optimizer.py
+++ optimizer.py
@@ -1,5 +1,5 @@
 import torch
-
+# Gray Wolf Optimization Algorithm Optimizer class
 class Optimizer:
 
     def __init__(self, pop_size, dim, lower, upper, max_fes, device):
@@ -12,22 +12,44 @@ class Optimizer:
 
     def optimize(self, func):
 
-        # Initialize population on GPU
-        population = torch.empty(
-            self.pop_size, self.dim, device=self.device
-        ).uniform_(self.lower, self.upper)
+        wolves = torch.empty(self.pop_size, self.dim, device=self.device)\
+            .uniform_(self.lower, self.upper)
 
         FEs = 0
         best_value = float("inf")
 
         while FEs < self.max_fes:
 
-            fitness = func(population)
+            fitness = func(wolves)
             FEs += self.pop_size
 
-            current_best = torch.min(fitness).item()
-            best_value = min(best_value, current_best)
+            best_value = min(best_value, torch.min(fitness).item())
 
-            ### Algorithm to be added 
+            idx = torch.argsort(fitness)
+            alpha = wolves[idx[0]].unsqueeze(0)
+            beta = wolves[idx[1]].unsqueeze(0)
+            delta = wolves[idx[2]].unsqueeze(0)
+
+            a = 2 - 2 * (FEs / self.max_fes)
+
+            r1 = torch.rand_like(wolves)
+            r2 = torch.rand_like(wolves)
+            A1 = 2*a*r1 - a
+            C1 = 2*r2
+            X1 = alpha - A1 * torch.abs(C1*alpha - wolves)
+
+            r1 = torch.rand_like(wolves)
+            r2 = torch.rand_like(wolves)
+            A2 = 2*a*r1 - a
+            C2 = 2*r2
+            X2 = beta - A2 * torch.abs(C2*beta - wolves)
+
+            r1 = torch.rand_like(wolves)
+            r2 = torch.rand_like(wolves)
+            A3 = 2*a*r1 - a
+            C3 = 2*r2
+            X3 = delta - A3 * torch.abs(C3*delta - wolves)
+
+            wolves = torch.clamp((X1 + X2 + X3)/3, self.lower, self.upper)
 
         return best_value
